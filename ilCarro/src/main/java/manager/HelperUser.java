82dae77231f89febddab91274b9053a3bd5ff8c9
JonatanIL <jonataneitan@gmail.com>
2022-06-28 01:20:57 +0300

    hw4

---
+++ ilCarro/src/main/java/manager/HelperUser.java
@@ -0,0 +1,47 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class HelperUser extends HelperBase{
+
+
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+    public void openLoginForm() {
+        WebElement loginTab = wd.findElement(By.xpath("//a[text()=' Log in ']"));
+        loginTab.click();
+    }
+
+    public void submitLogin() {
+        wd.findElement(By.cssSelector("button[type='submit']")).click();
+    }
+
+
+    public void fillLoginForm(String email, String password) {
+
+        // find +click+clear+sendKey
+        type(By.xpath("//input[@id='email']"), email);
+
+        // find +click+clear+sendKey
+        type(By.xpath("//input[@id='password']"), password);
+    }
+
+    public boolean isLogged() {
+        // sign out present? --> logged
+        List<WebElement > list =wd.findElements(By.xpath("//a[@href='/logout?url=%2Fsearch']"));
+        return list.size()>0;
+    }
+
+    public void logout() {
+        click(By.xpath("//a[@href='/logout?url=%2Fsearch']"));
+    }
+
+    public void submitButtonLogIn() {
+        click(By.xpath("//button[@class='positive-button ng-star-inserted']"));
+    }
+}
--- ilCarro/src/test/java/tests/LoginTests.java
+++ ilCarro/src/test/java/tests/LoginTests.java
@@ -1,18 +1,32 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
-    @Test
-    public void successLogin (){
-        //3. open form
-        openLoginForm();
-        //4. fill form+valid data
-        fillLoginForm("jonataneitan@gmail.com", "Jonatan1986");
-        // 5.submit login
-        submitLogin();
+    @BeforeMethod
+    public void preCondition() {
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
+    }
 
+    @Test
+    public void aLoginSuccess() {
+
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("jonataneitan@gmail.com", "Jonatan1986");
+        app.getHelperUser().submitLogin();
+        app.getHelperUser().submitButtonLogIn();
+
+    }
+
+    @Test
+        public void zLoginNegativeTestsWrongEmail() {
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("jonataneitangmail.com", "Jonatan1986");
+        app.getHelperUser().submitLogin();
     }
 
 }
