1517c6362f11a5ac70721d7a3921f381b8da889e
JonatanIL <jonataneitan@gmail.com>
2022-07-31 21:00:14 +0300

    AddComitToJenkins

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -45,6 +45,8 @@ public class HelperUser extends HelperBase{
         type(By.id("lastName"), user.getLastName());
         type(By.id("email"), user.getEmail());
         type(By.id("password"), user.getPassword());
+        //forJenkins
+        
     }
 
     public void checkPolicy() {
